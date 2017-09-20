d591ec72418a573e2427d0807fb399bb710d207b
tarilabs <matteo.mortari@gmail.com>
2017-09-20 18:02:08 +0200

    Version with default classpath resources.

--- dmn-demo-201709wb/src/test/java/org/drools/dmn_demo_201709wb/BasicTest.java
+++ dmn-demo-201709wb/src/test/java/org/drools/dmn_demo_201709wb/BasicTest.java
@@ -1,14 +1,12 @@
 package org.drools.dmn_demo_201709wb;
 
 import org.junit.Test;
-import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.manager.RuntimeEnvironment;
 import org.kie.api.runtime.manager.RuntimeEnvironmentBuilder;
 import org.kie.api.runtime.manager.RuntimeManager;
 import org.kie.api.runtime.manager.RuntimeManagerFactory;
-import org.kie.internal.io.ResourceFactory;
 import org.kie.internal.runtime.manager.context.EmptyContext;
 
 public class BasicTest {
@@ -16,9 +14,7 @@ public class BasicTest {
     @Test
     public void test_basic() {
         RuntimeEnvironment environment = RuntimeEnvironmentBuilder.Factory.get()
-                                                                          .newDefaultBuilder()
-                                                                          .addAsset(ResourceFactory.newClassPathResource("org/drools/dmn_demo_201709wb/simple process routing with DMN.bpmn2"), ResourceType.BPMN2)
-                                                                          .addAsset(ResourceFactory.newClassPathResource("org/drools/dmn_demo_201709wb/DMN presentation simplified.dmn"), ResourceType.DMN)
+                                                                          .newClasspathKmoduleDefaultBuilder()
                                                                           .persistence(false)
                                                                           .get();
 
