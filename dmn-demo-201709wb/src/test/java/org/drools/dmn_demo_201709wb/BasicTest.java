0731b93d3461d0034afb6dd43c5eb5b79a1a39f2
tarilabs <matteo.mortari@gmail.com>
2017-09-20 22:15:32 +0200

    WORKING.

--- dmn-demo-201709wb/src/test/java/org/drools/dmn_demo_201709wb/BasicTest.java
+++ dmn-demo-201709wb/src/test/java/org/drools/dmn_demo_201709wb/BasicTest.java
@@ -1,30 +1,36 @@
 package org.drools.dmn_demo_201709wb;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jbpm.test.JbpmJUnitBaseTestCase;
 import org.junit.Test;
+import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
-import org.kie.api.runtime.manager.RuntimeEnvironment;
-import org.kie.api.runtime.manager.RuntimeEnvironmentBuilder;
 import org.kie.api.runtime.manager.RuntimeManager;
-import org.kie.api.runtime.manager.RuntimeManagerFactory;
+import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.internal.runtime.manager.context.EmptyContext;
 
-public class BasicTest {
+public class BasicTest extends JbpmJUnitBaseTestCase {
 
     @Test
     public void test_basic() {
-        RuntimeEnvironment environment = RuntimeEnvironmentBuilder.Factory.get()
-                                                                          .newClasspathKmoduleDefaultBuilder()
-                                                                          .persistence(false)
-                                                                          .get();
+        Map<String, ResourceType> resources = new HashMap<>();
+        resources.put("org/drools/dmn_demo_201709wb/simple process routing with DMN.bpmn2", ResourceType.BPMN2);
+        resources.put("org/drools/dmn_demo_201709wb/DMN presentation simplified.dmn", ResourceType.DMN);
+        RuntimeManager manager = createRuntimeManager(resources, "BasicTest");
+        RuntimeEngine engine = getRuntimeEngine(EmptyContext.get());
+        KieSession ksession = engine.getKieSession();
 
-        RuntimeManager manager = RuntimeManagerFactory.Factory.get().newSingletonRuntimeManager(environment);
-
-        RuntimeEngine runtime = manager.getRuntimeEngine(EmptyContext.get());
-
-        KieSession kieSession = runtime.getKieSession();
-
-        kieSession.startProcess("dmn-demo-201709wb.simpleprocessroutingwithDMN", null);
+        Map<String, Object> application = new HashMap<>();
+        application.put("Name", "Matteo");
+        application.put("Age", 35);
+        application.put("Country", "IT");
+        application.put("Employed", true);
+        Map<String, Object> processVariables = new HashMap<>();
+        processVariables.put("Application", application);
+        ProcessInstance processInstance = ksession.startProcess("dmn-demo-201709wb.simpleprocessroutingwithDMN", processVariables);
 
     }
 }
