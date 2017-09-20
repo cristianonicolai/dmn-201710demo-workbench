9f082b03a86130c85c02b1d0a87fa2ad3a814a94
tarilabs <matteo.mortari@gmail.com>
2017-09-20 17:35:41 +0200

    WIP

--- dmn-demo-201709wb/src/test/java/org/drools/dmn_demo_201709wb/BasicTest.java
+++ dmn-demo-201709wb/src/test/java/org/drools/dmn_demo_201709wb/BasicTest.java
@@ -1,11 +1,29 @@
 package org.drools.dmn_demo_201709wb;
 
 import org.junit.Test;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeEnvironment;
+import org.kie.api.runtime.manager.RuntimeEnvironmentBuilder;
+import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.manager.RuntimeManagerFactory;
+import org.kie.internal.runtime.manager.context.EmptyContext;
 
 public class BasicTest {
 
     @Test
     public void test_basic() {
+        RuntimeEnvironment environment = RuntimeEnvironmentBuilder.Factory.get()
+                                                                          .newClasspathKmoduleDefaultBuilder()
+                                                                          .get();
+
+        RuntimeManager manager = RuntimeManagerFactory.Factory.get().newSingletonRuntimeManager(environment);
+
+        RuntimeEngine runtime = manager.getRuntimeEngine(EmptyContext.get());
+
+        KieSession kieSession = runtime.getKieSession();
+
+        kieSession.startProcess("dmn-demo-201709wb.simpleprocessroutingwithDMN", null);
 
     }
 }
